7919d8a57ef14616c2b2b19f8194cf3f9346d5e1
Carl Kadie <carlk@msn.com>
2026-02-16 14:36:21 -0800

    Refactor audio player configuration and update sample rate handling for digit clips

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@
 
 use defmt::info;
 use defmt_rtt as _;
-use device_envoy::audio_player::{
-    AtEnd, AudioClipSource, VOICE_22050_HZ, Volume, audio_player, pcm_clip, resampled_type,
-};
+use device_envoy::audio_player::{AtEnd, VOICE_22050_HZ, Volume, audio_player, pcm_clip};
 use device_envoy::ir::{IrKepler, IrKeplerStatic, KeplerButton};
 use embassy_executor::Spawner;
 use panic_probe as _;
@@ -15,7 +13,7 @@ audio_player! {
         data_pin: PIN_8,
         bit_clock_pin: PIN_9,
         word_select_pin: PIN_10,
-        sample_rate_hz: 10_000,
+        sample_rate_hz: VOICE_22050_HZ,
         pio: PIO1,
         max_volume: Volume::spinal_tap(11),
     }
@@ -23,94 +21,97 @@ audio_player! {
 
 pcm_clip! {
     Digit0 {
-        sample_rate_hz: VOICE_22050_HZ,
+        source_sample_rate_hz: VOICE_22050_HZ,
+        target_sample_rate_hz: IrVoicePlayer::SAMPLE_RATE_HZ,
         file: "../data/audio/digits/0_22050.s16",
     }
 }
 
 pcm_clip! {
     Digit1 {
-        sample_rate_hz: VOICE_22050_HZ,
+        source_sample_rate_hz: VOICE_22050_HZ,
+        target_sample_rate_hz: IrVoicePlayer::SAMPLE_RATE_HZ,
         file: "../data/audio/digits/1_22050.s16",
     }
 }
 
 pcm_clip! {
     Digit2 {
-        sample_rate_hz: VOICE_22050_HZ,
+        source_sample_rate_hz: VOICE_22050_HZ,
+        target_sample_rate_hz: IrVoicePlayer::SAMPLE_RATE_HZ,
         file: "../data/audio/digits/2_22050.s16",
     }
 }
 
 pcm_clip! {
     Digit3 {
-        sample_rate_hz: VOICE_22050_HZ,
+        source_sample_rate_hz: VOICE_22050_HZ,
+        target_sample_rate_hz: IrVoicePlayer::SAMPLE_RATE_HZ,
         file: "../data/audio/digits/3_22050.s16",
     }
 }
 
 pcm_clip! {
     Digit4 {
-        sample_rate_hz: VOICE_22050_HZ,
+        source_sample_rate_hz: VOICE_22050_HZ,
+        target_sample_rate_hz: IrVoicePlayer::SAMPLE_RATE_HZ,
         file: "../data/audio/digits/4_22050.s16",
     }
 }
 
 pcm_clip! {
     Digit5 {
-        sample_rate_hz: VOICE_22050_HZ,
+        source_sample_rate_hz: VOICE_22050_HZ,
+        target_sample_rate_hz: IrVoicePlayer::SAMPLE_RATE_HZ,
         file: "../data/audio/digits/5_22050.s16",
     }
 }
 
 pcm_clip! {
     Digit6 {
-        sample_rate_hz: VOICE_22050_HZ,
+        source_sample_rate_hz: VOICE_22050_HZ,
+        target_sample_rate_hz: IrVoicePlayer::SAMPLE_RATE_HZ,
         file: "../data/audio/digits/6_22050.s16",
     }
 }
 
 pcm_clip! {
     Digit7 {
-        sample_rate_hz: VOICE_22050_HZ,
+        source_sample_rate_hz: VOICE_22050_HZ,
+        target_sample_rate_hz: IrVoicePlayer::SAMPLE_RATE_HZ,
         file: "../data/audio/digits/7_22050.s16",
     }
 }
 
 pcm_clip! {
     Digit8 {
-        sample_rate_hz: VOICE_22050_HZ,
+        source_sample_rate_hz: VOICE_22050_HZ,
+        target_sample_rate_hz: IrVoicePlayer::SAMPLE_RATE_HZ,
         file: "../data/audio/digits/8_22050.s16",
     }
 }
 
 pcm_clip! {
     Digit9 {
-        sample_rate_hz: VOICE_22050_HZ,
+        source_sample_rate_hz: VOICE_22050_HZ,
+        target_sample_rate_hz: IrVoicePlayer::SAMPLE_RATE_HZ,
         file: "../data/audio/digits/9_22050.s16",
     }
 }
 
-static DIGIT0_NARROWBAND: resampled_type!(Digit0, IrVoicePlayer::SAMPLE_RATE_HZ) =
-    Digit0::pcm_clip().with_resampled();
-static DIGIT1_NARROWBAND: resampled_type!(Digit1, IrVoicePlayer::SAMPLE_RATE_HZ) =
-    Digit1::pcm_clip().with_resampled();
-static DIGIT2_NARROWBAND: resampled_type!(Digit2, IrVoicePlayer::SAMPLE_RATE_HZ) =
-    Digit2::pcm_clip().with_resampled();
-static DIGIT3_NARROWBAND: resampled_type!(Digit3, IrVoicePlayer::SAMPLE_RATE_HZ) =
-    Digit3::pcm_clip().with_resampled();
-static DIGIT4_NARROWBAND: resampled_type!(Digit4, IrVoicePlayer::SAMPLE_RATE_HZ) =
-    Digit4::pcm_clip().with_resampled();
-static DIGIT5_NARROWBAND: resampled_type!(Digit5, IrVoicePlayer::SAMPLE_RATE_HZ) =
-    Digit5::pcm_clip().with_resampled();
-static DIGIT6_NARROWBAND: resampled_type!(Digit6, IrVoicePlayer::SAMPLE_RATE_HZ) =
-    Digit6::pcm_clip().with_resampled();
-static DIGIT7_NARROWBAND: resampled_type!(Digit7, IrVoicePlayer::SAMPLE_RATE_HZ) =
-    Digit7::pcm_clip().with_resampled();
-static DIGIT8_NARROWBAND: resampled_type!(Digit8, IrVoicePlayer::SAMPLE_RATE_HZ) =
-    Digit8::pcm_clip().with_resampled();
-static DIGIT9_NARROWBAND: resampled_type!(Digit9, IrVoicePlayer::SAMPLE_RATE_HZ) =
-    Digit9::pcm_clip().with_resampled();
+// todo00 `IrVoicePlayerAudioClipSource` us a trait and name is long.
+const DIGITS: [&'static IrVoicePlayerAudioClipSource; 10] = [
+    &Digit0::adpcm256_clip_from(Digit0::pcm_clip()),
+    &Digit1::pcm_clip(),
+    &Digit2::pcm_clip(),
+    &Digit3::pcm_clip(),
+    &Digit4::pcm_clip(),
+    &Digit5::pcm_clip(),
+    &Digit6::pcm_clip(),
+    &Digit7::pcm_clip(),
+    &Digit8::pcm_clip(),
+    &Digit9::pcm_clip(),
+];
 
 const SPINAL_TAP_MIN: u8 = 0;
 const SPINAL_TAP_MAX: u8 = 11;
@@ -152,28 +153,15 @@ async fn main(spawner: Spawner) -> ! {
         SPINAL_TAP_INIT
     };
     ir_voice_player.set_volume(Volume::spinal_tap(spinal_tap_level));
-    let digits: [&'static dyn AudioClipSource<{ IrVoicePlayer::SAMPLE_RATE_HZ }>; 10] = [
-        &DIGIT0_NARROWBAND,
-        &DIGIT1_NARROWBAND,
-        &DIGIT2_NARROWBAND,
-        &DIGIT3_NARROWBAND,
-        &DIGIT4_NARROWBAND,
-        &DIGIT5_NARROWBAND,
-        &DIGIT6_NARROWBAND,
-        &DIGIT7_NARROWBAND,
-        &DIGIT8_NARROWBAND,
-        &DIGIT9_NARROWBAND,
-    ];
-
     info!("Audio startup check: speaking 0");
-    ir_voice_player.play([digits[0]], AtEnd::Stop);
+    ir_voice_player.play([DIGITS[0]], AtEnd::Stop);
 
     loop {
         let button = ir_kepler.wait_for_press().await;
         match button {
             KeplerButton::Num(number) => {
                 info!("Playing digit {}", number);
-                if let Some(digit_audio_clip) = digits.get(number as usize).copied() {
+                if let Some(digit_audio_clip) = DIGITS.get(number as usize).copied() {
                     ir_voice_player.play([digit_audio_clip], AtEnd::Stop);
                 }
             }
